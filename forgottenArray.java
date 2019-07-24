7c1e208b272040977045b25d4a183bf26ac4a9f8
chrishamlin98 <52384782+chrishamlin98@users.noreply.github.com>
2019-07-24 06:05:09 -0700

    Create forgottenArray.java

---
+++ forgottenArray.java
@@ -0,0 +1,17 @@
+public class arrayPractice {
+
+  public class static void main(String[] args) {
+    int[] array = {1, 3, 5, 7, 11, 13};
+
+    for (int y: array){
+      Changes(nums.y);
+    }
+  }
+  public static void Changes(int nums[]){
+    for (int Changes = 0; Changes < nums.length; Changes++){
+      Changes(x += 15);
+    }
+
+  }
+
+}
