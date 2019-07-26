1bb3333e674c118a188397255458dbf227d01df7
chrishamlin98 <52384782+chrishamlin98@users.noreply.github.com>
2019-07-26 06:46:43 -0700

    Update forgottenArray.java

--- forgottenArray.java
+++ forgottenArray.java
@@ -2,13 +2,20 @@ public class arrayPractice {
 
   public class static void main(String[] args) {
     int[] array = {1, 3, 5, 7, 11, 13};
+    Changes(array);
 
-    for (int y: array){
-      Changes(nums.y);
-    }
+    for (int y: array)
+    // This is the part I messed up -
+    // I put Changes(nums.y) and I put it in the wrong place;
+      System.out.print(y);
   }
+      // This is the part I messed up -
+      // I put Changes(nums.y) and I put it in the wrong place;
   public static void Changes(int nums[]){
-    for (int Changes = 0; Changes < nums.length; Changes++){
+    // In the for loop I keep making the pointer connect
+    // to the method and paramaters, but it is separate!
+    // This is NOT correct for (int Changes = 0; Changes < nums.length; Changes++){
+    for (int i = 0; i < nums.length; i++) {
       Changes(x += 15);
     }
 
