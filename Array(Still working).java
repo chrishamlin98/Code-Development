64dbf2ab48025b6b6e89c9d5b8da83992db5b163
chrishamlin98 <52384782+chrishamlin98@users.noreply.github.com>
2019-08-04 07:03:16 -0700

    Array practice

--- Array(Still working).java
+++
@@ -1,9 +0,0 @@
-public static void main(String[] args) {
-  int[] array = {1,2,3,4,5};
-  change(array);
-
-  for (y, array)
-    System.out.print 
-
-
-}
---
+++ Array3.java
@@ -0,0 +1,19 @@
+public static void main(String[] args) {
+  int[] array = {1,2,3,4,5};
+  change(array);
+
+//I keep forgetting what the 2nd term in the enhanced loop is
+// also I forgot to include the "int" before the y
+  for (int y, array)
+    System.out.print(y);
+
+  public static void change(int x[]){
+    for (int i = 0; i < x.length; i++)
+      //now I'm pausing at this point
+      //I keep trying to put x for the change and I need to put
+      //the i, since it is pointing to the new array
+      change[i] += 10;
+  }
+
+
+}
